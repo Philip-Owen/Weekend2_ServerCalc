458431f860d7d136a8c0241c3f435bf28d317421
Philip-Owen <31570473+Philip-Owen@users.noreply.github.com>
2017-12-26 15:12:44 -0600

    added comments to server.js and calculation.js

--- server/modules/calculation.js
+++ server/modules/calculation.js
@@ -1,3 +1,6 @@
+
+// Used to construct Calculation objects to be pushed into calcArray
+// and sent to the client side.
 class Calculation {
     constructor(result, equation) {
         this.result = result;
@@ -5,16 +8,21 @@ class Calculation {
     }
 }
 
+// Stores Calculation objects.
 calcArray = [];
 
-let result = 0;
-let pastCalc = 0;
 
+// Performs mathmatical calculations on the objects that are sent from
+// client side.
 function performCalc(inputObject) {
+    let result = 0;
+    let pastCalc = 0;
     numOne = Number(inputObject.firstNum);
     numTwo = Number(inputObject.secondNum);
     operator = inputObject.operator;
 
+    // evaluates operator passed in through object to determin which
+    // calculation to perform.
     switch (operator) {
         case '+':
             result = numOne + numTwo
@@ -39,10 +47,12 @@ function performCalc(inputObject) {
     }
 }
 
+// Resets calcArray to an empty array.
 function resetAll() {
     calcArray.length = 0;
 }
 
+// Exports functions and array to be used on server.js
 module.exports = {
     performCalc: performCalc,
     calcArray: calcArray,
--- server/server.js
+++ server/server.js
@@ -9,21 +9,24 @@ const app = express();
 app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({extended:true}));
 
+// Receives object from client side ajax POST, and performs calculations
 app.post('/calculation', (req,res) => {
     calculation.performCalc(req.body);
     res.sendStatus(201);
 });
 
+// Delete request to clear history from the page, changes server side history
+// array to zero.
 app.delete('/clearResults', (req,res) => {  
     calculation.resetAll();
     res.send(calculation.calcArray);
 });
 
+// Returns an array of objects to the client side.
 app.get('/calculation', (req,res) =>  {
     // console.log('in calc', calculation.calcArray);
     res.send(calculation.calcArray);
 });
-
 
 // server listener
 const port = 8088;
